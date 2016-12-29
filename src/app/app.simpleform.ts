fe4b9be188569ec71b9c9fceda995ed758d8c80e
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 00:55:48 +0530

    add simple form

---
+++ src/app/app.simpleform.ts
@@ -0,0 +1,17 @@
+/**
+* @Author: Tushar Agarwal <Tushar>
+* @Date:   2016-12-30T00:35:20+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.simpleform.ts
+* @Last modified by:   Tushar
+* @Last modified time: 2016-12-30T00:43:13+05:30
+*/
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'simple-form',
+  template: '<h1></h1>'
+})
+export class SimpleFormComponent{
+
+}
