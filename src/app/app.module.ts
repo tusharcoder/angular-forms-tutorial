5dfdd3cd5432289c73336345c9fdc4924d52016c
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 01:10:31 +0530

    add components to the app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,33 @@
 * @Email:  tamyworld@gmail.com
 * @Filename: app.module.ts
 * @Last modified by:   Tushar
-* @Last modified time: 2016-12-30T00:41:40+05:30
+* @Last modified time: 2016-12-30T01:01:27+05:30
 */
 
 
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import {SimpleFormComponent} from './app.simpleform'
+import {ComplexFormComponent} from './app.complexform'
+import {FormValidationComponent} from './app.formvalidations'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleFormComponent,
+    ComplexFormComponent,
+    FormValidationComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
--- src/app/app.simpleform.ts
+++ src/app/app.simpleform.ts
@@ -4,13 +4,13 @@
 * @Email:  tamyworld@gmail.com
 * @Filename: app.simpleform.ts
 * @Last modified by:   Tushar
-* @Last modified time: 2016-12-30T00:43:13+05:30
+* @Last modified time: 2016-12-30T01:09:33+05:30
 */
 import {Component} from '@angular/core';
 
 @Component({
   selector: 'simple-form',
-  template: '<h1></h1>'
+  template: '<h1>Simple Form</h1>'
 })
 export class SimpleFormComponent{
 
