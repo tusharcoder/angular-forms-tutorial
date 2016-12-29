42734bed7a6fdd03175fbaa68f41f1726ab06cd5
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 00:55:25 +0530

    add component for form validation

---
+++ src/app/app.formvalidations.ts
@@ -0,0 +1,18 @@
+/**
+* @Author: Tushar Agarwal <Tushar>
+* @Date:   2016-12-30T00:50:21+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.formvalidation.ts
+* @Last modified by:   Tushar
+* @Last modified time: 2016-12-30T00:52:06+05:30
+*/
+import {Component} from '@angular/core';
+
+@Component({
+selector:'form-validation',
+template:'<h1>Form Validation</h1>'
+})
+
+export class FormValidationComponent{
+
+}
