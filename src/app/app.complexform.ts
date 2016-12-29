557526097d9122af1643f4d42cea1feabe6f08f2
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 01:27:17 +0530

    add form as class property

--- src/app/app.complexform.ts
+++ src/app/app.complexform.ts
@@ -4,14 +4,31 @@
 * @Email:  tamyworld@gmail.com
 * @Filename: app.complexform.ts
 * @Last modified by:   Tushar
-* @Last modified time: 2016-12-30T00:49:10+05:30
+* @Last modified time: 2016-12-30T01:26:49+05:30
 */
 import {Component} from '@angular/core';
+import {FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
 selector:'complex-form',
 templateUrl:'./app.complexform.html'
 })
 export class ComplexFormComponent{
+  complexForm: FormGroup;
 
+  constructor(fb: FormBuilder){
+    this.complexForm=fb.group({
+      'firstName':'',
+      'lastName':'',
+      'gender':'Male',
+      'hiking':false,
+      'running':false,
+      'swimming':false,
+    });
+  }
+
+  submitForm(value:any):void{
+    console.log('reactive form data');
+    console.log(value);
+  }
 }
