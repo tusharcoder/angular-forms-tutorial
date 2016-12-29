73125224d7eb2cf4014c675eddd1131230125c80
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 00:56:03 +0530

    add author

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,14 @@
+/**
+* @Author: Tushar Agarwal <Tushar>
+* @Date:   2016-12-30T00:20:14+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.module.ts
+* @Last modified by:   Tushar
+* @Last modified time: 2016-12-30T00:41:40+05:30
+*/
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
