917c960960af5bb3ded4cef6ce58ab8b103adfe5
tushar_scaledesk <tushar@scaledesk.com>
2016-12-30 00:52:44 +0530

    add complex form component

---
+++ src/app/app.complexform.ts
@@ -0,0 +1,17 @@
+/**
+* @Author: Tushar Agarwal <Tushar>
+* @Date:   2016-12-30T00:40:02+05:30
+* @Email:  tamyworld@gmail.com
+* @Filename: app.complexform.ts
+* @Last modified by:   Tushar
+* @Last modified time: 2016-12-30T00:49:10+05:30
+*/
+import {Component} from '@angular/core';
+
+@Component({
+selector:'complex-form',
+templateUrl:'./app.complexform.html'
+})
+export class ComplexFormComponent{
+
+}
